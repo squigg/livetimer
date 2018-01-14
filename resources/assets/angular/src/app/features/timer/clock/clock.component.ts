137ba9c0368482c7fd93e29fa68f350ebaeda593
Squigg <github@squigg.co.uk>
2018-01-14 16:14:47 +0000

    Centre aligned and implemented noHours view on clock component

--- resources/assets/angular/src/app/features/timer/clock/clock.component.ts
+++ resources/assets/angular/src/app/features/timer/clock/clock.component.ts
@@ -9,7 +9,7 @@ export class ClockComponent implements OnInit {
 
     @Input() time: number;
     @Input() paused: boolean;
-    @Input() showHours: boolean;
+    @Input() showHours = true;
     protected hours: number;
     protected minutes: number;
     protected seconds: number;
@@ -18,10 +18,17 @@ export class ClockComponent implements OnInit {
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        const time = parseInt(changes['time'].currentValue);
-        this.hours = Math.floor(time / (60 * 60));
-        this.minutes = Math.floor(time / 60) - this.hours * 60;
-        this.seconds = Math.floor(time) - this.minutes * 60;
+        if (changes['time']) {
+            const time = parseInt(changes['time'].currentValue);
+            if (this.showHours) {
+                this.hours = Math.floor(time / (60 * 60));
+                this.minutes = Math.floor(time / 60) - this.hours * 60;
+            }
+            else {
+                this.minutes = Math.floor(time / 60);
+            }
+            this.seconds = Math.floor(time) - this.minutes * 60;
+        }
     }
 
     ngOnInit() {
