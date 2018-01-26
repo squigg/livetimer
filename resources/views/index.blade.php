10a4460178b69c4b5a5566ef323b78234acf37d9
Squigg <github@squigg.co.uk>
2018-01-26 02:15:46 +0000

    Remove redundant view files

--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-@section('content')
-  <h1>Timers</h1>
-  <ul>
-    @foreach($timers as $timer)
-      <li>{{ $timer->name }}</li>
-    @endforeach
-  </ul>
-@stop
--- resources/views/timer.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('master')
-@section('content')
-  <h1>Timer {{ $timer->name }}</h1>
-  <app-root></app-root>
-  <script type="text/javascript" src="/dist/main.bundle.js"></script>
-@stop
