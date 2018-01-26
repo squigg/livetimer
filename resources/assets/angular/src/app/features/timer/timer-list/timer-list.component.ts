ec2541655109b9f8a4b8e8a45a07b2c13bd4cf2e
Squigg <github@squigg.co.uk>
2018-01-26 01:59:13 +0000

    Basic Timer List added

--- resources/assets/angular/src/app/features/timer/timer-list/timer-list.component.ts
+++ resources/assets/angular/src/app/features/timer/timer-list/timer-list.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerHttpService } from "../../../services/timer/timer-http.service";
+import { Timer } from "../../../models/timer";
 
 @Component({
-    selector: 'app-timerlist',
+    selector: 'app-timer-list',
     templateUrl: './timer-list.component.html',
     styleUrls: ['./timer-list.component.scss']
 })
 export class TimerListComponent implements OnInit {
 
-    constructor() {
+    private timers: Timer[];
+    private name: string;
+
+    constructor(private timerHttpService: TimerHttpService) {
     }
 
     ngOnInit() {
+        this.getTimers();
     }
 
+    async getTimers() {
+        this.timers = await this.timerHttpService.getTimers();
+    }
+
+    async createTimer() {
+        if (!this.name) return;
+        await this.timerHttpService.create(this.name);
+        await this.getTimers();
+        this.name = '';
+    }
 }
--- resources/assets/angular/src/app/features/timer/timer.module.ts
+++ resources/assets/angular/src/app/features/timer/timer.module.ts
@@ -7,20 +7,25 @@ import { SharedModule } from "../../shared/shared.module";
 import { ClockComponent } from './clock/clock.component';
 import { TimerAdminComponent } from "./timer-admin/timer-admin.component";
 import { TimerDisplayComponent } from "./timer-display/timer-display.component";
+import { TimerListComponent } from "./timer-list/timer-list.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
         FormsModule,
+        RouterModule
     ],
     exports: [
         TimerAdminComponent,
         TimerDisplayComponent,
+        TimerListComponent,
     ],
     declarations: [
         TimerAdminComponent,
         TimerDisplayComponent,
+        TimerListComponent,
         ClockComponent,
     ]
 })
