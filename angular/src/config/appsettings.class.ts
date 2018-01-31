704afb5065bc26c0a72c0fac9e0a23c59b2e2469
Squigg <github@squigg.co.uk>
2018-01-31 18:47:03 +0000

    Add sounds to config

--- angular/src/config/appsettings.class.ts
+++ angular/src/config/appsettings.class.ts
@@ -3,5 +3,15 @@ import { environment } from '../environments/environment';
 export class AppSettings {
     public static API_ROOT = environment.base_api_url;
     public static PUSHER_KEY = environment.pusher_key;
-    public static SOUNDS = ['woop_woop', 'railroad_crossing_bell'];
+    public static SOUNDS = [
+        'woop_woop',
+        'railroad_crossing_bell',
+        'air_horn_1',
+        'alert_5',
+        'bleep',
+        'door_chime',
+        'air_horn_2',
+        'beep',
+        'buzz',
+    ];
 }
