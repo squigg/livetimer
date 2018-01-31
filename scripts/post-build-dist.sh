c431b4a27341f1275bca8e838406927f1accd1f2
Squigg <github@squigg.co.uk>
2018-01-31 00:36:05 +0000

    Add post build script for renames

---
+++ scripts/post-build-dist.sh
@@ -0,0 +1,7 @@
+pushd public/dist
+mv polyfills.*.js polyfills.bundle.js
+mv main.*.js main.bundle.js
+mv styles.*.css styles.bundle.css
+mv vendor.*.js vendor.bundle.js
+mv inline.*.js inline.bundle.js
+popd
