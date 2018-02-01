f9b111c94157e1f338fcafa8e8990410e1e8c943
Squigg <github@squigg.co.uk>
2018-02-01 00:23:22 +0000

    Update prod URL

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    base_api_url: 'http://livetimer.squigg.co.uk:15491/api',
+    base_api_url: 'http://livetimer.squigg.co.uk/api',
     pusher_key: 'ffc8403a2e5fbd7e0cfc',
 };
